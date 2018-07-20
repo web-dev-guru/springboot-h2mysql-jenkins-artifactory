9219bad086066320b7a458da7907d43994291c23
web-dev-guru <wesley.wei.java@gmail.com>
2018-07-19 22:18:15 -0400

    change h2 from file to mem

--- src/main/java/com/aws/devops/DevopsApplication.java
+++ src/main/java/com/aws/devops/DevopsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevopsApplication {
-//test
+//test  11
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsApplication.class, args);
 	}
